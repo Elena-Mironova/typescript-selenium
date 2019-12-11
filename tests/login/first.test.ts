b3007a6e4f3b7479de4d2558c783c83a3a61189e
Elena-Mironova <mironova_elenag@mail.ru>
2019-12-11 20:07:26 +0300

    Vk. Brunoyam.

--- tests/login/first.test.ts
+++ tests/login/first.test.ts
@@ -30,25 +30,54 @@ describe("Login form", function() {
     browser = new SeleniumUtils(driver);
   });
 
-  it("Positive test", async function() {
-    // browser.go(App.url); // переход
-    // await page.isLoad();  // проверяем, что загрузилась
 
-    driver.get('https://m.vk.com/');
+  it("Negative test VK", async function() {    
+    driver.get('https://m.vk.com/'); 
     await driver.sleep(2000);
     await driver.findElement(By.css('[name="email"]')).sendKeys('+79211402028');
     await driver.sleep(1000);
     await driver.findElement(By.css('[type="password"]')).sendKeys('test');
     await driver.sleep(1000);
-    await driver.findElement(By.css('[type="submit"]')).click(); // кнопка "войти"
+    await driver.findElement(By.css('[type="submit"]')).click();
+    await driver.sleep(1000);
+    await driver.findElement(By.css('.service_msg.service_msg_warning'));
+  });
 
 
+  it("Positive test Проверка сертификата", async function() {    
+    driver.get('https://brunoyam.com/verify');
+    await driver.sleep(1000);
+    await driver.findElement(By.css('form[action="/verify"] [name="fio"]')).sendKeys('Шарина Юлия Валерьевна');
+    await driver.findElement(By.css('form[action="/verify"] [name="number"]')).sendKeys('TE250-1725');
+    await driver.findElement(By.css('form[action="/verify"] button[type="submit"]')).click();
+    await driver.sleep(1000);
+    await driver.findElement(By.css('.certificate'));
+  });
+
+
+  it("Negative test Проверка сертификата", async function() {
+    driver.get('https://brunoyam.com/verify');
+    await driver.sleep(1000);
+    await driver.findElement(By.css('form[action="/verify"] [name="fio"]')).sendKeys('Шарина Юлия Валерьевна');
+    await driver.findElement(By.css('form[action="/verify"] [name="number"]')).sendKeys('TE250-1');
+    await driver.findElement(By.css('form[action="/verify"] button[type="submit"]')).click();
+    await driver.sleep(1000);
+    await driver.findElement(By.css('.v-alert.alert.alert-danger'));
+  });
+
+
+  
+
+  //it("Positive test", async function() {
+    // browser.go(App.url); // переход
+    // await page.isLoad();  // проверяем, что загрузилась
     // await browser.keys(page.email(), App.user.login);
     // await browser.keys(page.password(), App.user.password); 
     // await browser.click(page.submit());
     // await calendarPage.isLoad();
     // await assert.equal(await calendarPage.isPage(), true);
-  });
+  // });
+
 
   // it("Negative test", async function() {    // ctrl k+c;   ctrl k+u (закомментировать; отменить комментирование)
   //   debugger;
@@ -60,6 +89,7 @@ describe("Login form", function() {
   //   await page.isLoad();
   //   await assert.equal(await page.isPage(), true);
   // });
+
 
   after(() => driver && driver.quit());
 });
